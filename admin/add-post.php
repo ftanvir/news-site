1736d8e28b8a4a965744a45270956ba7b1271eeb
ftanvir <ft.tanvir551@gmail.com>
2023-06-04 01:35:35 +0600

    category dropdown added

--- admin/add-post.php
+++ admin/add-post.php
@@ -7,7 +7,7 @@
              </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form -->
-                  <form  action="" method="POST" enctype="multipart/form-data">
+                  <form  action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
                       <div class="form-group">
                           <label for="post_title">Title</label>
                           <input type="text" name="post_title" class="form-control" autocomplete="off" required>
@@ -19,7 +19,20 @@
                       <div class="form-group">
                           <label for="exampleInputPassword1">Category</label>
                           <select name="category" class="form-control">
-                              <option value="" selected> Select Category</option>
+                            <option disabled> Select Category</option>
+                            <?php
+                                include "config.php";
+
+                                $sql = "SELECT * FROM category";
+                                $result = mysqli_query($conn, $sql) or die("Query Failed");
+
+                                if(mysqli_num_rows($result) > 0){
+                                    while($row = mysqli_fetch_assoc($result)){
+                                        echo "<option value=''>{$row['category_name']}</option>";
+                                    }
+                                }
+                                ?>
+                              
                           </select>
                       </div>
                       <div class="form-group">
@@ -29,6 +42,7 @@
                       <input type="submit" name="submit" class="btn btn-primary" value="Save" required />
                   </form>
                   <!--/Form -->
+        
               </div>
           </div>
       </div>
